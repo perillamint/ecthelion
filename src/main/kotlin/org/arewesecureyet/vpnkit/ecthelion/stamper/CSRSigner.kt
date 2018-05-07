a1d81d2c83cdcfd8458e3e1f51f16454400f1d02
perillamint <perillamint@quendi.moe>
2018-05-07 22:09:09 +0900

    Try to complete CSRSigner

--- src/main/kotlin/org/arewesecureyet/vpnkit/ecthelion/stamper/CSRSigner.kt
+++ src/main/kotlin/org/arewesecureyet/vpnkit/ecthelion/stamper/CSRSigner.kt
@@ -1,13 +1,18 @@
 package org.arewesecureyet.vpnkit.ecthelion
 
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo
+import org.bouncycastle.cert.X509v3CertificateBuilder
+import org.bouncycastle.crypto.util.PrivateKeyFactory
 import org.bouncycastle.crypto.util.PublicKeyFactory
+import org.bouncycastle.operator.DefaultDigestAlgorithmIdentifierFinder
 import org.bouncycastle.operator.DefaultSignatureAlgorithmIdentifierFinder
 import org.bouncycastle.pkcs.PKCS10CertificationRequest
 import java.security.KeyPair
 
 class CSRSigner {
-    private lazyinit var certAlgo;
+    private lazyinit var sigAlgoId;
+    private lazyinit var digAlgoId;
+    private lazyinit var caPrivKeyParam;
     constructor (hashAlgo: String, caKey: KeyPair) {
         val sigAlgo = caKey.private.algorithm;
 
@@ -16,14 +21,21 @@ class CSRSigner {
             throw IllegalArgumentException("ERROR: cannot sign with vulnerable algorithm.")
         }
 
-        certAlgo = DefaultSignatureAlgorithmIdentifierFinder()
+        sigAlgoId = DefaultSignatureAlgorithmIdentifierFinder()
                 .find(hashAlgo + "with" + sigAlgo);
+
+        digAlgoId = DefaultDigestAlgorithmIdentifierFinder()
+                .find(sigAlgoId);
+
+        caPrivKeyParam = PrivateKeyFactory.createKey(caKey.private.encoded);
     }
 
     fun signCSR(csr: PKCS10CertificationRequest) {
         val csrPubk = csr.subjectPublicKeyInfo;
-        val csrPubkParam = PublicKeyFactory.createKey(csrPubk);
+        val certBuilder = X509v3CertificateBuilder(
+                //
+        )
 
-        // TODO: Extract pubkey, build cert.
+        // TODO: build cert.
     }
 }
