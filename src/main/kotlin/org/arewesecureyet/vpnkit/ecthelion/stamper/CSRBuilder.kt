d04643e31f28e219ec651413d8d99d14c709b0ca
perillamint <perillamint@quendi.moe>
2018-05-13 22:37:05 +0900

    Finish CSRBuilder

--- src/main/kotlin/org/arewesecureyet/vpnkit/ecthelion/stamper/CSRBuilder.kt
+++ src/main/kotlin/org/arewesecureyet/vpnkit/ecthelion/stamper/CSRBuilder.kt
@@ -1,24 +1,39 @@
 package org.arewesecureyet.vpnkit.ecthelion.stamper
 
+import org.bouncycastle.asn1.ASN1ObjectIdentifier
 import org.bouncycastle.asn1.x500.X500NameBuilder
 import org.bouncycastle.asn1.x500.style.BCStyle
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder
+import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder
 import java.security.KeyPair
 
 class CSRBuilder {
-    private var state: String
+    private val sigAlgo: String;
     private val x500NameBuilder: X500NameBuilder
+    private val kp: KeyPair
     constructor(hashAlgo: String, keypair: KeyPair) {
         if(hashAlgo == "SHA1" || hashAlgo == "NONE" || keypair.private.algorithm == "DSA") {
             throw IllegalArgumentException("ERROR: cannot sign with vulnerable algorithm.")
         }
 
-        val sigAlgoName = keypair.private.algorithm
+        val kp = keypair
+        sigAlgo = hashAlgo + "with" + keypair.private.algorithm
         x500NameBuilder = X500NameBuilder(BCStyle.INSTANCE);
 
-        JcaContentSignerBuilder
     }
 
-    setRDN(nameX500NameStyle)
+    fun addRDN(oid: ASN1ObjectIdentifier, value: String) {
+        x500NameBuilder.addRDN(oid, value)
+    }
 
+    fun build() {
+        val p10builder = JcaPKCS10CertificationRequestBuilder(
+                x500NameBuilder.build(), kp.public
+        )
+
+        val csBuilder = JcaContentSignerBuilder(sigAlgo)
+        val signer = csBuilder.build(kp.private)
+
+        return p10builder.build(signer)
+    }
 }
